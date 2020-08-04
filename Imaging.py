b0cf512e1a3e2fbae584d03af38d0af50587d409
Damian Bemben <bembendamian@yahoo.co.uk>
2020-08-04 23:03:25 +0100

    Initial commit

---
+++ Imaging.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug  3 21:54:08 2020
+
+@author: bembe
+"""
+
+from PIL import Image, ImageFilter
+import numpy as np
+
+im = Image.open('greyscale.png') 
+pixel_values = list(im.getdata())
+print(pixel_values[0])
+pix = im.load()
+
+pixels = []
+
+# Get the width and hight of the image for iterating over
+for x in range(im.size[0]):
+   for y in range(im.size[1]):
+       with open('image2.txt', 'a') as myfile:
+            myfile.write(str(pix[x,y][0]) + ",")       
